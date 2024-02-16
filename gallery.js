373edce9bf6749e7dd6f0ae42d8969fffbfc5e1d
prerana1821 <prerananawar1@gmail.com>
2024-02-17 04:17:46 +0530

    feat: all images retrieval to gallery

--- gallery.js
+++ gallery.js
@@ -1,8 +1,8 @@
 setTimeout(() => {
   if (db) {
     // videos retrieval
-    const transaction = db.transaction("video", "readonly");
-    const videoStore = transaction.objectStore("video");
+    const videoTransaction = db.transaction("video", "readonly");
+    const videoStore = videoTransaction.objectStore("video");
     const videoRequest = videoStore.getAll();
 
     videoRequest.addEventListener("success", (event) => {
@@ -20,6 +20,39 @@ setTimeout(() => {
         mediaGallery.innerHTML = `
             <div class="media">
                 <video src=${url} autoplay loop></video>
+            </div>
+            <div class="media-actions">
+                <div class="dowload">
+                    <img src="./icons/download.png" alt="Download Media" />
+                </div>
+                <div class="delete">
+                    <img src="./icons/delete.png" alt="Delete Media" />
+                </div>
+            </div>
+                `;
+
+        displayGallery.appendChild(mediaGallery);
+      });
+    });
+
+    // videos retrieval
+    const imageTransaction = db.transaction("image", "readonly");
+    const imageStore = imageTransaction.objectStore("image");
+    const imageRequest = imageStore.getAll();
+
+    imageRequest.addEventListener("success", (event) => {
+      let images = imageRequest.result;
+
+      let displayGallery = document.querySelector(".display-gallery");
+
+      images.forEach((image) => {
+        let mediaGallery = document.createElement("div");
+        mediaGallery.setAttribute("class", "media-gallery");
+        mediaGallery.setAttribute("id", image.id);
+
+        mediaGallery.innerHTML = `
+            <div class="media">
+                <img src=${image.url} alt=${image.id} />
             </div>
             <div class="media-actions">
                 <div class="dowload">
